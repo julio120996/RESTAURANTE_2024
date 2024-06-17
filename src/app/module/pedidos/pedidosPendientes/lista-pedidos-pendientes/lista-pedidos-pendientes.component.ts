b6cefaf82bb5e101d1d05577ee99259eda69f194
Grabiel1212 <136027575+Grabiel1212@users.noreply.github.com>
2024-06-17 18:47:08 -0500

    wew3e

--- src/app/module/pedidos/pedidosPendientes/lista-pedidos-pendientes/lista-pedidos-pendientes.component.ts
+++ src/app/module/pedidos/pedidosPendientes/lista-pedidos-pendientes/lista-pedidos-pendientes.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { Pedidosp } from '../../../../core/models/PedidoP';
 
 @Component({
   selector: 'app-lista-pedidos-pendientes',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule , TableModule],
   templateUrl: './lista-pedidos-pendientes.component.html',
   styleUrl: './lista-pedidos-pendientes.component.scss'
 })
+
+
 export class ListaPedidosPendientesComponent {
 
+  ButtonStyle={
+   
+    width : '2.3rem',
+    height:'2.3rem',
+    'margin-left':'1rem'
+  }
+
+  pedidoss : Pedidosp [] = [
+    {
+    
+    
+      id : 1,
+    nombre  : 'jose mena',
+    apellidos: 'Terrez carasco',
+    fecha: '12/6/2024',
+    menu: 'arroz cin pollo',
+    cantidad  : 2 ,
+    total : 58.9
+    } ,
+    
+  ]
 }
