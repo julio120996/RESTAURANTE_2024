79199270fc5854ab0ff8c7581f2f25d4d6d0ed77
JulioCesar <111259560+julio120996@users.noreply.github.com>
2024-06-17 21:13:41 -0500

    agregando

---
+++ src/app/core/models/PedidoP.ts
@@ -0,0 +1,13 @@
+export interface Pedidosp{
+
+    id : number;
+    nombre  : string;
+    apellidos: String;
+    fecha: String ;
+    menu: String;
+    cantidad  : number;
+    total : number ;
+    
+
+
+ }
--- src/app/module/pedidos/pedidosPendientes/lista-pedidos-pendientes/lista-pedidos-pendientes.component.ts
+++ src/app/module/pedidos/pedidosPendientes/lista-pedidos-pendientes/lista-pedidos-pendientes.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { Pedidosp } from '../../../../core/models/PedidoP';
 
 @Component({
   selector: 'app-lista-pedidos-pendientes',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule , TableModule],
   templateUrl: './lista-pedidos-pendientes.component.html',
   styleUrl: './lista-pedidos-pendientes.component.scss'
 })
+
+
 export class ListaPedidosPendientesComponent {
 
+  ButtonStyle={
+   
+    width : '2.3rem',
+    height:'2.3rem',
+    'margin-left':'1rem'
+  }
+
+  pedidoss : Pedidosp [] = [
+    {
+    
+    
+      id : 1,
+    nombre  : 'jose mena',
+    apellidos: 'Terrez carasco',
+    fecha: '12/6/2024',
+    menu: 'arroz cin pollo',
+    cantidad  : 2 ,
+    total : 58.9
+    } ,
+    {
+    
+    
+      id : 2,
+    nombre  : 'Julio',
+    apellidos: 'Carrasco',
+    fecha: '12/6/2024',
+    menu: 'Chaufa',
+    cantidad  : 8 ,
+    total : 100.5
+    } ,
+    {
+    
+    
+      id : 3,
+    nombre  : 'Estefany',
+    apellidos: 'Lara Serrano',
+    fecha: '17/6/2024',
+    menu: 'ceviche',
+    cantidad  : 1 ,
+    total : 12.9
+    } ,
+    {
+    
+    
+      id : 4,
+    nombre  : 'Maria',
+    apellidos: 'Barboza Luño',
+    fecha: '12/6/2024',
+    menu: 'Amburgesa',
+    cantidad  : 1 ,
+    total : 10.2
+    } 
+    
+  ]
 }
